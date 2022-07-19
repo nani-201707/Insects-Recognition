04fad955f8926fc3bdcc6b751c7ac9011838e17b
nani-201707 <109607712+nani-201707@users.noreply.github.com>
2022-07-19 20:15:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:'https://www.nyckel.com/v1/functions/pxvrvn451z7vtd44/invoke',
+        url:'https://www.nyckel.com/v1/functions/gdvllacugxhgjtom/invoke',
         type: 'post',
         data: formdata,
         contentType: false,
